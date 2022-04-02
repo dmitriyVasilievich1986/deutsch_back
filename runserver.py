dec3bc8b467147532fe9919ff30b4b173a36a2a2
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-02 18:49:36 +0300

    add runserver.py file

---
+++ runserver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from django.core.management import execute_from_command_line
+from os import environ
+from argparse import ArgumentParser
+import re
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+
+def set_enviroments():
+    with open(".env", "r") as f:
+        for l in f.readlines():
+            regular = re.match(r'^.*?=', l)
+            if regular:
+                value = re.sub(r"^" + regular.group(0), "", l)
+                environ.setdefault(
+                    re.sub(r"=$", "", regular.group(0)),
+                    re.sub(r"\n$", "", value)
+                )
+
+
+def runserver():
+    execute_from_command_line(["deutsch", "runserver", "0.0.0.0:3000"])
+
+
+if __name__ == '__main__':
+    set_enviroments()
+    runserver()
