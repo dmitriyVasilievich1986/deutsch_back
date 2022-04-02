0e72ce6dc29229ed2fc95cc721b14c447257fcae
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-02 18:50:45 +0300

    small update settings file

--- deutsch/settings.py
+++ deutsch/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'main',
 ]
 
@@ -103,3 +104,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
