0aa17aa92c02cd8a7cd8dc41a4f41ada56b1874e
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-03 18:21:42 +0300

    update runserver script add db connection check

--- runserver.py
+++ runserver.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from django.core.management import execute_from_command_line
-from os import environ, path
+from django.db.utils import OperationalError
 from argparse import ArgumentParser
+from django.db import connections
+from os import environ, path
+from time import sleep
+import logging
 import re
 
 
@@ -23,12 +27,24 @@ def set_enviroments():
                 )
 
 
-def runserver():
+def execute_commands():
     execute_from_command_line(["deutsch", "makemigrations"])
     execute_from_command_line(["deutsch", "migrate"])
     execute_from_command_line(["deutsch", "runserver", "0.0.0.0:3000"])
 
 
+def runserver():
+    for _ in range(5):
+        try:
+            connections["default"].cursor()
+            logging.info(f"connected successfuly")
+            return execute_commands
+        except OperationalError as e:
+            logging.error(e)
+        sleep(10)
+    raise OperationalError
+
+
 if __name__ == '__main__':
     set_enviroments()
-    runserver()
+    runserver()()
