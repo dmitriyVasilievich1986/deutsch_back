3d1f184a685531fa9a1c1c8438d579e1e438fc26
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-04-24 13:45:49 +0300

    update wort model add path and description fields

--- main/models.py
+++ main/models.py
@@ -11,7 +11,9 @@ class ThemeModel(models.Model):
 
 class WortModel(models.Model):
     wort = models.CharField(max_length=150, null=False, unique=True)
+    path = models.CharField(max_length=150, null=True, unique=True)
     translate = models.CharField(max_length=150, null=False)
+    description = models.TextField(null=True, blank=True)
     group = models.ForeignKey(
         on_delete=models.CASCADE,
         related_name="wort",
