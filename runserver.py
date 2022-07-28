00ade6585c8275f1d98acb64f2f531c6a3de3ed3
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2022-07-28 08:02:51 +0200

    change loading configuration

--- runserver.py
+++ runserver.py
@@ -3,38 +3,23 @@ from django.core.management import execute_from_command_line
 from django.db.utils import OperationalError
 from argparse import ArgumentParser
 from django.db import connections
-from os import environ, path
+from dotenv import load_dotenv
+from os import environ
 from time import sleep
 import logging
-import re
 
 
 def parse_args():
     parser = ArgumentParser()
 
 
-def set_enviroments():
-    environ.setdefault("DJANGO_SETTINGS_MODULE", "deutsch.settings")
-    if not path.exists(".env"):
-        return
-    with open(".env", "r") as f:
-        for l in f.readlines():
-            regular = re.match(r'^.*?=', l)
-            if regular:
-                value = re.sub(r"^" + regular.group(0), "", l)
-                environ.setdefault(
-                    re.sub(r"=$", "", regular.group(0)),
-                    re.sub(r"\n$", "", value)
-                )
-
-
 def execute_commands():
-    execute_from_command_line(["deutsch", "makemigrations"])
-    execute_from_command_line(["deutsch", "migrate"])
+    execute_from_command_line([__name__, "makemigrations"])
+    execute_from_command_line([__name__, "migrate"])
 
     HOST = environ.get("HOST", "0.0.0.0")
     PORT = environ.get("PORT", "3000")
-    execute_from_command_line(["deutsch", "runserver", f"{HOST}:{PORT}"])
+    execute_from_command_line([__name__, "runserver", f"{HOST}:{PORT}"])
 
 
 def runserver():
@@ -55,5 +40,6 @@ def runserver():
 
 
 if __name__ == '__main__':
-    set_enviroments()
+    load_dotenv()
+    load_dotenv("config.env")
     runserver()()
